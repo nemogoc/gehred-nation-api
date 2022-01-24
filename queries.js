fcd0eab615d8281d24c32e4a964b04180bb4428d
nemogoc <1668286+nemogoc@users.noreply.github.com>
2022-01-24 13:47:03 -0600

    add basic user CRUD template

---
+++ queries.js
@@ -0,0 +1,76 @@
+const Pool = require('pg').Pool
+
+//TODO: read docs, I think there's a better way to config these by naming the env vars
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+})
+
+const getUsers = (req, res) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
+}
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id)
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
+}
+
+const createUser = (req, res) => {
+  const { name, email } = req.body
+
+  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(201).send(`User added with ID: ${results.insertId}`)
+  })
+}
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id)
+  const { name, email } = req.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      res.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+}
