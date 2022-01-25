ae497da9183a28ff7475850d2fe7572b4a1b01d5
nemogoc <1668286+nemogoc@users.noreply.github.com>
2022-01-25 17:02:34 -0600

    combine get user(s) queries

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
+app.get('/users/:id', db.getUsers)
 app.post('/users', db.createUser)
 // app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
--- queries.js
+++ queries.js
@@ -10,67 +10,66 @@ const pool = new Pool({
 })
 
 const getUsers = (req, res) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).json(results.rows)
-  })
-}
-
-const getUserById = (req, res) => {
-  const id = parseInt(req.params.id)
-
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).json(results.rows)
-  })
+  if (req.params.id === undefined) {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      res.status(200).json(results.rows)
+    })
+  }
+  else {
+    const id = parseInt(req.params.id)
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      res.status(200).json(results.rows)
+    })
+  }
 }
 
 const createUser = (req, res) => {
-  const { firstname, lastname, email, dob, street, city, state, zip, phone } = req.body
+    const { firstname, lastname, email, dob, street, city, state, zip, phone } = req.body
 
-  pool.query('INSERT INTO users (firstname, lastname, email, dob, street, city, state, zip, phone) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id', [firstname, lastname, email, dob, street, city, state, zip, phone], (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(201).send(`User added with ID: ${results.rows[0].id}`)
-  })
-}
+    pool.query('INSERT INTO users (firstname, lastname, email, dob, street, city, state, zip, phone) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id', [firstname, lastname, email, dob, street, city, state, zip, phone], (error, results) => {
+      if (error) {
+        throw error
+      }
+      res.status(201).send(`User added with ID: ${results.rows[0].id}`)
+    })
+  }
 
-// const updateUser = (req, res) => {
-//   const id = parseInt(req.params.id)
-//   const { name, email } = req.body
+  // const updateUser = (req, res) => {
+  //   const id = parseInt(req.params.id)
+  //   const { name, email } = req.body
 
-//   pool.query(
-//     'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-//     [name, email, id],
-//     (error, results) => {
-//       if (error) {
-//         throw error
-//       }
-//       res.status(200).send(`User modified with ID: ${id}`)
-//     }
-//   )
-// }
+  //   pool.query(
+  //     'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+  //     [name, email, id],
+  //     (error, results) => {
+  //       if (error) {
+  //         throw error
+  //       }
+  //       res.status(200).send(`User modified with ID: ${id}`)
+  //     }
+  //   )
+  // }
 
-const deleteUser = (req, res) => {
-  const id = parseInt(req.params.id)
+  const deleteUser = (req, res) => {
+    const id = parseInt(req.params.id)
 
-  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).send(`User deleted with ID: ${id}`)
-  })
-}
+    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      res.status(200).send(`User deleted with ID: ${id}`)
+    })
+  }
 
-module.exports = {
-  getUsers,
-  getUserById,
-  createUser,
-  // updateUser,
-  deleteUser,
-}
+  module.exports = {
+    getUsers,
+    createUser,
+    // updateUser,
+    deleteUser,
+  }
